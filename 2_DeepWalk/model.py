c481812ee4bef119c71bee10ab57a488e11139eb
Li Jinliang <necerlixin@outlook.com>
2024-10-23 15:19:12 +0800

    增加使用KL散度的模型

--- 2_DeepWalk/model.py
+++ 2_DeepWalk/model.py
@@ -34,3 +34,37 @@ class SkipGramHierarchicalSoftmaxModel(nn.Module):
             logits_selected.view(B, -1), tree_codes.view(B, -1).float()
         )
         return loss
+
+
+class SkipGramKLModel(nn.Module):
+    def __init__(self, nodes_num: int, embedding_dim: int) -> None:
+        super().__init__()
+        self.nodes_num = nodes_num
+        self.embedding_size = embedding_dim
+        self.embedding = nn.Embedding(
+            num_embeddings=nodes_num,
+            embedding_dim=embedding_dim,
+        )
+        self.cls = nn.Linear(
+            in_features=embedding_dim,
+            out_features=nodes_num,
+        )
+
+    def forward(
+        self,
+        input_nodes,
+        prob_dist,
+    ):
+        ############################################
+        # 由于没有使用哈夫曼树，所以就没使用MASK了 #
+        ############################################
+        # target_ndoes:[b,h]
+        x = self.embedding(input_nodes)  # [b,e]
+        B = x.shape[0]
+        logits = self.cls(x)  # [b,n]
+        prob = torch.softmax(logits, dim=-1)
+        loss = F.kl_div(prob_dist.log(), prob, reduction="batchmean")
+        # path_nodes [b, s, h]
+        # tree_nodes [b, s, h]
+
+        return loss
